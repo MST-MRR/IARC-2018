35b0b8026bc7b6277e43bde73a8bc7065623b04e
christopher-o-toole <cdocq9@mst.edu>
2017-08-17 12:37:12 -0500

    VideoReader is now an iterable+changed sample code to reflect that

---
+++ examples/face_detection/train_face_detector.py
@@ -0,0 +1,15 @@
+############################################
+# Multi-Rotor Robot Design Team
+# Missouri University of Science and Technology
+# Summer 2017
+# Christopher O'Toole
+
+'''Example showing how to retrain one of the face detector's cascade stages with your own dataset.'''
+
+import sys
+sys.path.append('../..')
+
+import mrrdt_vision
+import cv2
+
+
