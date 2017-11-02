8488ee1bb15bcc7ee9b5b81436238ead12d845d2
Daniel Herzberg <dphz9f@mst.edu>
2017-11-01 22:22:57 -0500

    Updated for drone-mounted LIDAR

--- Collision/ScanseRun.py
+++ Collision/ScanseRun.py
@@ -1,19 +1,20 @@
 from time import sleep
 from altScan import LIDAR
-from dronekit import *
+import dronekit
+
+vehicle = dronekit.connect("/dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00", wait_ready=True)
 
 def distance_sensor(min_dist, max_dist, current_dist, sector):
-    msg = message_factory.command_long_encode(0, 0 , mavutil.mavlink.DISTANCE_SENSOR, 
+    msg = vehicle.message_factory.command_long_encode(0, 0 , 0, 
                             0, min_dist, max_dist, current_dist, 0, 0, sector,0)
     print "%s" %msg
-    send_mavlink(msg)
+    vehicle.send_mavlink(msg)
 
 def send_lidar_message(min_dist, max_dist, current_dist, sector):
-
     distance = data[0]
     sensor_rotation = data[1]
     print("Distance :" + str(distance) + " Quad: " + str(sensor_rotation))
-    message = self.vehicle.message_factory.distance_sensor_encode(
+    message = vehicle.message_factory.distance_sensor_encode(
     0,                                             # time since system boot, not used
     min_dist,                                      # min distance cm
     max_dist,                                      # max distance cm
@@ -37,7 +38,7 @@ while(1):   #constantly grab data
         print "\nFor sector " + (str)(secval)
         endVal = min(5, len(sector))
         for val in range(0,endVal):
-            #print sector[val][0]
+            print "Sending message"
             distance_sensor(10, 300, sector[val][1], sector[val][2])
         secval += 1
 
