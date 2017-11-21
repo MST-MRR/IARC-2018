07b6c784fcf3177de172e038a330c5298c5e9068
Daniel Herzberg <dphz9f@mst.edu>
2017-11-21 15:13:59 -0600

    Bogus

--- Collision/altscan.py
+++ Collision/altscan.py
@@ -6,23 +6,22 @@ import math
 from sweeppy import Sweep
 
 class LIDAR():
-  MIN_SAFE_DISTANCE = 50.0
-  MAX_SAFE_DISTANCE = 15000.0
+  MIN_SAFE_DISTANCE = 10.0
+  MAX_SAFE_DISTANCE = 300.0
   QUADRANT_SIZE = 45.0
 
   def __init__(self):
     #self.lidar_sensor = "/dev/cu.usbserial-DO00867Q" #this is for Mac OS X 
     self.lidar_sensor = "/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_DO00867Q-if00-port0" #this is for Linux
     self.sweep = Sweep(self.lidar_sensor)
-    print "SCANSE INIT"
-    self.sweep = None
+    #print "SCANSE INIT"
+    #self.sweep = None
 
   def connect_to_lidar(self):
     self.sweep.__enter__()
     # Starts scanning as soon as the motor is ready
     self.sweep.set_motor_speed(6)
     self.sweep.set_sample_rate(1000)
-
     self.sweep.start_scanning()
 
   #We encountered errors with getting points in each sector, so this method
@@ -35,10 +34,10 @@ class LIDAR():
   def get_lidar_data(self):
     # Starts scanning as soon as the motor is ready
     lidar_data = [ [], [], [], [], [], [], [], [] ]
-
+    #print "check"
     # get_scans is coroutine-based generator lazily returning scans
     for scan in itertools.islice(self.sweep.get_scans(), 1): #for every sector
-      
+      all_points = []
       sector_points = []
       sector_lists = [ [], [], [], [], [], [], [], [] ]
       
@@ -48,30 +47,18 @@ class LIDAR():
         
         distance = sample.distance
         angle_deg = (sample.angle / 1000.0) % 360.0
-        angle_rad = math.radians(sample.angle / 1000.0)
-        sector = abs((((angle_deg % 360.0)-360) // self.QUADRANT_SIZE)+1)
-        sector = (8 - sector) % 8 #The LIDAR sensor returns angle data in the wrong direction
-                                  #We correct this here so that it can be sent as a proper MAVLink command.
+        sector = (int)((angle_deg % 360.0) // self.QUADRANT_SIZE)
         
         if (distance < self.MAX_SAFE_DISTANCE and distance > self.MIN_SAFE_DISTANCE):
-          sector_lists[(int)(sector)].append( (distance, angle_deg) )
-          #print (distance, angle_deg)
-          
-    
-    for k in range(0, 8): #force at least 5 elements into list, they will be filtered out
-        sector_lists[k] = [len(sector_lists[k]), (3000, -1.0)]*5
-        
-    #Begin processing
-    for i in range(0, 8):
-      sector_lists[i].sort()
-      #print sector_lists[i]
-      first_five = []
-      for j in range(0, 5):
-        first_five.append(sector_lists[i][j])
-      print first_five
-      lidar_data[i]=first_five
+          sector_lists[(int)(sector)].append( (distance, angle_deg, sector) )
 
-    print("\n")
+    for point_list in sector_lists:
+      point_list.sort()
+
+    for sector_index in range(0,8):
+      endVal = min(10, len(sector_lists[sector_index]))
+      for index in range(0,endVal):
+        lidar_data[sector_index].append(sector_lists[sector_index][index])
 
     return lidar_data
 
