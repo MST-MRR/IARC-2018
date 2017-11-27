2543ddad5862bce9d34111e41b092a09e953ad33
Tanner Winkelman <winkelmantanner@gmail.com>
2017-11-26 20:18:45 -0600

    Added pygazebo topic subscription to IARCFly.py.

--- Flight/IARCFly.py
+++ Flight/IARCFly.py
@@ -20,9 +20,72 @@ HORIZONTAL_ROOMBA_POSITION = 0.5
 # Ratio of image from left of image
 VERTICAL_ROOMBA_POSITION = 1.0 / 3.0
 
+
+
+
+# from Lucas' Test1_v4.py file
+import trollius
+from trollius import From
+import sys
+from PIL import Image
+import numpy as np
+
+import pygazebo
+import pygazebo.msg.image_stamped_pb2
+
+
+# Yes, a global variable.  I don't know how else to do it. -Tanner
+image = np.ndarray(shape = (0,0,0))
+
+
+
+@trollius.coroutine
+def publish_loop():
+    print ( 'nooo2' )
+    
+    manager = yield From(pygazebo.connect()) 
+    print ( 'nooo3' )
+
+    def callback(data):
+        print ( 'Got mail.' )
+        message = pygazebo.msg.image_stamped_pb2.ImageStamped.FromString(data)
+        width = message.image.width
+        height = message.image.height
+        datalist = list ( message.image.data )
+        npdatalist = np.array(datalist)
+        npdatalist = np.reshape(npdatalist , (height, width, 3))
+
+        image = npdatalist # added by Tanner
+
+        
+    
+    print ( 'nooo4' )
+    # originally /gazebo/default/iris_demo/gimbal_small_2d/tilt_link/camera/image
+    subscriber = manager.subscribe('/gazebo/default/iris/iris_demo/gimbal_small_2d/tilt_link/camera/image',
+                     'gazebo.msgs.ImageStamped',
+                     callback)
+    print ( 'nooo5' )
+
+    yield From(subscriber.wait_for_connection())
+    print ( 'nooo6' )
+    while(True):
+        print ( 'fire' )
+        yield From(trollius.sleep(1.00))
+    print(dir(manager))
+
+import logging
+
+logging.basicConfig()
+
+# Deleted Trollius stuff here.  Put Trollius stuff at bottom. -Tanner
+
+# end of code from Lucas' Test1_v4.py file -Tanner
+
+
+
 def InitializeScreenScraping ( ) :
     # Some screen scraping stuff here
-
+    pass
 
 def InitializeScreenScraping ( ) :
     # Some screen scraping stuff here
@@ -66,6 +129,7 @@ def FindBestOrientedRoomba ( Roombas ) :
         if RoombaOrientation < BestOrientation :
             BestOrientation = RoombaOrientation
             BestRoomba = Roomba
+    return BestRoomba
 
 def ScreenScrape ( WindowName ) :
     ImageWindow = GetWindow ( WindowName )
@@ -106,33 +170,51 @@ def MoveToFollowRoomba ( Roomba , Image , t ) :
     
 
 ############## The main program ##################
+@trollius.coroutine
+def main():
+    # Do neccesary initializations
+    InitializeScreenScraping ( )
+    t = InitializeConnection ( )
+    InitializeDrone ( t )
 
-# Do neccesary initializations
-InitializeScreenScraping ( )
-t = InitializeConnection ( )
-InitializeDrone ( t )
+    # Take off
+    InitialTakeoff ( t )
 
-# Take off
-InitialTakeoff ( t )
+    # Fly to middle of field
+    InitialFlyToMiddle ( t )
 
-# Fly to middle of field
-InitialFlyToMiddle ( t )
-
-# Stop
-ReadyHover ( t )
+    # Stop
+    ReadyHover ( t )
 
 
-# At this point, we expect the roombas to start moving ..
+    # At this point, we expect the roombas to start moving ..
 
-# Now chase after roomba whose orientation is closest to drone orientation in image
-# We will eventually change this infinite loop to check for out of bounds
-while ( True == True ) :
-    Image = ScreenScrape ( GAZEBO_IMAGE_WINDOW_NAME )
-    
-    # Not sure of Vision's api.  Hopefully its something close to this.
-    DroneRoombaDetector = RoombaDetector ( )
-    TargetRoombas = DroneRoombaDetector . detect ( Image )
-    TargetRoomba = FindBestOrientedRoomba ( TargetRoombas )
-    
-    # Now set drone to move to correct position
-    MoveToFollowRoomba ( TargetRoomba , Image , t )
+    # Now chase after roomba whose orientation is closest to drone orientation in image
+    # We will eventually change this infinite loop to check for out of bounds
+    while ( True == True ) :
+        # Tanner made image lowercase because we have a library named Image    
+        #image = ScreenScrape ( GAZEBO_IMAGE_WINDOW_NAME )
+        img = Image.fromarray(image, 'RGB')
+        img.save('my2.png')
+        print("Tanner was here")
+        
+        # Not sure of Vision's api.  Hopefully its something close to this.
+        DroneRoombaDetector = RoombaDetector ( )
+        TargetRoombas = DroneRoombaDetector . detect ( image )
+        TargetRoomba = FindBestOrientedRoomba ( TargetRoombas )
+        
+        # Now set drone to move to correct position
+        MoveToFollowRoomba ( TargetRoomba , image , t )
+
+        yield From(trollius.sleep(1.00)) # added by Tanner
+
+
+
+
+
+tasks = []
+tasks.append(trollius.Task(publish_loop()))
+tasks.append(trollius.Task(main()))
+loop = trollius.get_event_loop()
+loop.run_until_complete(trollius.wait(tasks))
+
