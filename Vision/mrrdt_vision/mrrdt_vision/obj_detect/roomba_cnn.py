5cbcb1bb89961cc83eb1b4f188c94e184ffb59c0
christopher-o-toole <cdocq9@mst.edu>
2017-12-02 14:27:28 -0600

    Latest model+tensorflow integrated with Roomba class

--- Vision/mrrdt_vision/mrrdt_vision/obj_detect/roomba_cnn.py
+++ Vision/mrrdt_vision/mrrdt_vision/obj_detect/roomba_cnn.py
@@ -34,40 +34,19 @@ TEST_VIDEO_PATH = '../data/IARC.mp4'
 
 CWD_PATH = os.getcwd()
 DATA_DIR = 'ssd'
-PATH_TO_CKPT = os.path.join(CWD_PATH, DATA_DIR, 'ssd_300_roomba_v1.pb')
+PATH_TO_CKPT = os.path.join(CWD_PATH, DATA_DIR, 'ssd_300_roomba_v2.pb')
 PATH_TO_LABELS = os.path.join(CWD_PATH, DATA_DIR, 'object-detection.pbtext')
 NUM_CLASSES = 1
+DEFAULT_THRESHOLD = .5
+ROOMBA_CATEGORY_NAME = 'roomba'
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES)
 category_index = label_map_util.create_category_index(categories)
-
-def detect_objects(image_np, sess, detection_graph):
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-    scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-    (boxes, scores, classes, num_detections) = sess.run(
-        [boxes, scores, classes, num_detections],
-        feed_dict={image_tensor: image_np_expanded})
-
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        np.squeeze(scores),
-        category_index,
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    
-    return image_np
+name_to_id = {properties['name']: id for id, properties in category_index.items()}
 
 class Roomba():
-    def __init__(self, bounding_box, center, orientation):
+    def __init__(self, bounding_box, center, orientation=np.zeros(2)):
         self._bounding_box = bounding_box
         self._center = center
         self._orientation = orientation
@@ -87,15 +66,17 @@ class Roomba():
         return self._orientation
 
     def draw(self, img, show_orientation=True):
-        x_min, y_min, x_max, y_max = self.bounding_box
+        y_min, x_min, y_max, x_max = self.bounding_box
         cv2.rectangle(img, (x_min, y_min), (x_max, y_max), self._bounding_box_color, self._bounding_box_thickness)
 
         if show_orientation:
             cv2.arrowedLine(img, tuple(self.center.astype(int)), tuple((self.center+ARROW_LENGTH*self.orientation).astype(int)), ARROW_COLOR, ARROW_THICKNESS)
 
 class RoombaDetector():
-    def __init__(self):
+    def __init__(self, threshold=DEFAULT_THRESHOLD):
         self._detection_graph = tf.Graph()
+        self._threshold = threshold
+        self._target_id = name_to_id[ROOMBA_CATEGORY_NAME]
 
         with self._detection_graph.as_default():
             graph_def = tf.GraphDef()
@@ -115,6 +96,18 @@ class RoombaDetector():
     def detection_graph(self):
         return self._detection_graph
 
+    @property
+    def threshold(self):
+        return self._threshold
+
+    @property
+    def object_id(self):
+        return self._target_id
+
+    @threshold.setter
+    def threshold(self, value):
+        self._threshold = value
+
     def detect(self, img):
         rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         image_np_expanded = np.expand_dims(rgb_img, axis=0)
@@ -124,19 +117,21 @@ class RoombaDetector():
         scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
         classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
         num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
-
+        
         (boxes, scores, classes, num_detections) = self.session.run(
             [boxes, scores, classes, num_detections],
             feed_dict={image_tensor: image_np_expanded})
+        
+        idxs = np.where(np.bitwise_and(scores >= self.threshold, classes == self.object_id))
+        centers = np.zeros((len(idxs[0]), 2), np.int32)
 
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            img,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=8)
+        if len(idxs[0]) > 0:
+            h, w = img.shape[:2]
+            boxes = (boxes[idxs]*np.array([h, w, h, w])).astype(np.int32)
+            centers[:, 0] = np.sum(boxes[:, 1::2], axis=1)/2
+            centers[:, 1] = np.sum(boxes[:, ::2], axis=1)/2
+        
+        return [Roomba(box, center) for box, center in zip(boxes, centers)] if len(idxs[0]) > 0 else []
 
 if __name__ == '__main__':
     # unit test
@@ -149,16 +144,22 @@ if __name__ == '__main__':
             should_quit = False
 
             for frame in video:
+                frames += 1
+
                 if should_quit:
                     break
                 if frame is None:
                     break
+                if frames % 3 < 2:
+                    continue
                 
-                detector.detect(frame)
+                roombas = detector.detect(frame)
+
+                for roomba in roombas:
+                    roomba.draw(frame)
+                
                 window.show(frame)
                 should_quit = window.is_key_down(QUIT_KEY)
-
-                frames += 1
 
                 if (timer()-start >= 1):
                     window.set_title('%s (%d FPS)' % (TEST_VIDEO_PATH, frames))
