b83efef2dd282128466da049a2d6783a025704a5
firecannons198 <lawf48@mst.edu>
2017-12-08 14:59:20 -0600

    Make simple fixes to IARCFlyReal.py

--- Flight/IARCFlyReal.py
+++ Flight/IARCFlyReal.py
@@ -52,6 +52,12 @@ class SimpleDroneAI():
         self._detector = RoombaDetector()
         self._time_since_last_roomba = 0
         self._hovering = True
+        
+        ## start the service - also available as context manager
+        serv = pyrs.Service()
+
+        ## create a device from device id and streams of interest
+        cam = serv.Device(device_id = 0, streams = [pyrs.stream.ColorStream(fps = 60)])
 
     @property
     def hovering(self):
@@ -144,17 +150,8 @@ class SimpleDroneAI():
         """
         self._takeoff()
         
-        ## start the service - also available as context manager
-        serv = pyrs.Service()
-
-        ## create a device from device id and streams of interest
-        cam = serv.Device(device_id = 0, streams = [pyrs.stream.ColorStream(fps = 60)])
-        
         cam.wait_for_frames()
         self.update(cam.color)
-        
-        cam.stop()
-        serv.stop()
         
 
         while True:
@@ -163,3 +160,6 @@ class SimpleDroneAI():
 ai = SimpleDroneAI()
 loop = trollius.get_event_loop()
 loop.run_until_complete(ai.run())
+
+ai.cam.stop()
+ai.serv.stop()
