db9faddd6e2c0b8ba8091510c52edd3559f9e60a
christopher-o-toole <cdocq9@mst.edu>
2018-02-14 22:03:14 -0600

    Depth stream is now utilized

--- Flight/IARC_AI.py
+++ Flight/IARC_AI.py
@@ -20,6 +20,7 @@ from sklearn.preprocessing import normalize
 import pygazebo
 import pygazebo.msg.image_stamped_pb2
 import logging
+import argparse
 
 import trollius
 from trollius import From
@@ -46,12 +47,18 @@ class SimpleDroneAI():
     RESET_COMPLETE_EVENT_LOCATION = '/gazebo/default/reset_complete'
     # reset event type
     RESET_EVENT_TYPE = 'gazebo.msgs.GzString'
-    # camage image message location
-    CAMERA_MSG_LOCATION = '/gazebo/default/realsense_camera/rs/stream/color'
+    # color camage image message location
+    COLOR_CAMERA_MSG_LOCATION = '/gazebo/default/realsense_camera/rs/stream/color'
+    # depth camera image message location
+    DEPTH_CAMERA_MSG_LOCATION = '/gazebo/default/realsense_camera/rs/stream/depth'
+    # depth far clip
+    DEPTH_FAR_CLIP_MM = 10000
+    # depth near clip
+    DEPTH_NEAR_CLIP_MM = 300
     # camera message type
     CAMERA_MSG_TYPE = 'gazebo.msgs.ImageStamped'
     # altitude to hover at after takeoff in meters
-    TAKEOFF_HEIGHT = 1.5
+    TAKEOFF_HEIGHT = 2
     # maximum time spent on drone shutdown task
     DRONE_SHUTDOWN_TASK_TIMEOUT = 7
     # max speed (m/s)
@@ -74,6 +81,19 @@ class SimpleDroneAI():
             cv2.waitKey(1)
 
         return self._ardupilot_connection.update()
+
+    def _depth_image_handler(self, data):
+        message = pygazebo.msg.image_stamped_pb2.ImageStamped.FromString(data)
+        h, w = (message.image.height, message.image.width)
+        img = np.fromstring(message.image.data, dtype=np.uint16).reshape(h, w)
+        maximum = np.ones((h, w), dtype=np.float32)*SimpleDroneAI.DEPTH_COLOR_SCALE_MAX
+
+        if _DEBUG:
+            norm = (img.astype(np.float32)-img.min())/(img.max()-img.min())*255
+            gray = norm.astype(np.uint8)
+            color = cv2.applyColorMap(gray, cv2.COLORMAP_AUTUMN)
+            cv2.imshow('depth', color)
+            cv2.waitKey(1)
 
     def _event_handler(self, data):
         message = pygazebo.msg.image_stamped_pb2.ImageStamped.FromString(data)
@@ -106,13 +126,13 @@ class SimpleDroneAI():
         manager = yield From(pygazebo.connect()) 
         publisher = yield From(manager.advertise(SimpleDroneAI.RESET_COMPLETE_EVENT_LOCATION, SimpleDroneAI.RESET_EVENT_TYPE))
 
-        subscriber = manager.subscribe(SimpleDroneAI.CAMERA_MSG_LOCATION, SimpleDroneAI.CAMERA_MSG_TYPE, self._event_handler)
+        subscriber = manager.subscribe(SimpleDroneAI.COLOR_CAMERA_MSG_LOCATION, SimpleDroneAI.CAMERA_MSG_TYPE, self._event_handler)
+        depth_image_subscriber = manager.subscribe(SimpleDroneAI.DEPTH_CAMERA_MSG_LOCATION, SimpleDroneAI.CAMERA_MSG_TYPE, self._depth_image_handler)
         reset_event = manager.subscribe(SimpleDroneAI.RESET_EVENT_LOCATION, SimpleDroneAI.RESET_EVENT_TYPE, self._reset)
 
         try:
             yield From(subscriber.wait_for_connection())
-            self._tower.takeoff(SimpleDroneAI.TAKEOFF_HEIGHT*3)
-            self._tower.fly(FlightVector(3, 0, -5))
+            self._tower.takeoff(SimpleDroneAI.TAKEOFF_HEIGHT)
 
             while True:
                 if self._reset_ai:
@@ -144,31 +164,68 @@ class SimpleDroneAI():
         finally:
             cv2.destroyAllWindows()
 
+parser = argparse.ArgumentParser(description='Processes AI settings passed in')
+parser.add_argument('--test', '-T', type=int, help='test experimental features', default=1)
+args = parser.parse_args()
 
-with ArdupilotGazeboMonitor(speedup=3) as ardupilot_connection:
-    while True:
-        try:
-            while not ardupilot_connection.is_ready() or not ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
-                start = timer()
+if not args.test:
+    with ArdupilotGazeboMonitor(speedup=3) as ardupilot_connection:
+        while True:
+            try:
+                while not ardupilot_connection.is_ready() or not ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
+                    start = timer()
 
-                while ardupilot_connection.update() and not (start-timer()) >= READY_TO_FLY_TIMEOUT:
-                    if ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
-                        break
-            
-                if not ardupilot_connection.is_ready() or not ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
-                    ardupilot_connection.restart()
+                    while ardupilot_connection.update() and not (start-timer()) >= READY_TO_FLY_TIMEOUT:
+                        if ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
+                            break
+                
+                    if not ardupilot_connection.is_ready() or not ardupilot_connection.wait_for_ready_to_fly(timeout=DEFAULT_TIMEOUT):
+                        ardupilot_connection.restart()
 
-            ai = SimpleDroneAI(ardupilot_connection)
-            loop = trollius.get_event_loop()
-            loop.run_until_complete(ai.run())
-        except QuadcopterCrash:
-            ardupilot_connection.flush()
-            ardupilot_connection.reset()
-            ardupilot_connection.restart()
-        except ResetSimulatorException:
-            if ardupilot_connection.wait_for_gps_glitch(timeout=3):
+                ai = SimpleDroneAI(ardupilot_connection)
+                loop = trollius.get_event_loop()
+                loop.run_until_complete(ai.run())
+            except QuadcopterCrash:
+                ardupilot_connection.flush()
                 ardupilot_connection.reset()
-        except ArdupilotDisconnect:
-            ardupilot_connection.restart()
-        except KeyboardInterrupt:
-            break
+                ardupilot_connection.restart()
+            except ResetSimulatorException:
+                if ardupilot_connection.wait_for_gps_glitch(timeout=3):
+                    ardupilot_connection.reset()
+            except ArdupilotDisconnect:
+                ardupilot_connection.restart()
+            except KeyboardInterrupt:
+                break
+else:
+    def _depth_image_handler(data):
+        message = pygazebo.msg.image_stamped_pb2.ImageStamped.FromString(data)
+        h, w = (message.image.height, message.image.width)
+        img = np.fromstring(message.image.data, dtype=np.uint16).reshape(h, w)
+        zeros = np.zeros((h, w), dtype=np.uint16)
+        
+        if _DEBUG:
+            scale = (SimpleDroneAI.DEPTH_FAR_CLIP_MM-SimpleDroneAI.DEPTH_NEAR_CLIP_MM)
+            offset = SimpleDroneAI.DEPTH_NEAR_CLIP_MM
+            norm = np.maximum(img.astype(np.float32)-offset, zeros)/scale * 255
+            gray = norm.astype(np.uint8)
+            color = cv2.applyColorMap(gray, cv2.COLORMAP_AUTUMN)
+            cv2.imshow('depth', color)
+            cv2.waitKey(1)
+
+    @trollius.coroutine
+    def run():
+        manager = yield From(pygazebo.connect())
+        depth_image_subscriber = manager.subscribe(SimpleDroneAI.DEPTH_CAMERA_MSG_LOCATION, SimpleDroneAI.CAMERA_MSG_TYPE, _depth_image_handler)
+
+        try:
+            yield From(depth_image_subscriber.wait_for_connection())
+
+            while True:
+                yield From(trollius.sleep(0.01))
+        except KeyboardInterrupt as e:
+            pass
+        finally:
+            cv2.destroyAllWindows()
+
+    loop = trollius.get_event_loop()
+    loop.run_until_complete(run())
